4beeea89ce852c567803fcaaa21f2170052b6f66
wdyfy <wdy317138@outlook.com>
2019-03-06 00:00:32 -0700

    add Thymeleaf

---
+++ src/main/java/blog/springboot/controller/RouterController.java
@@ -0,0 +1,13 @@
+package blog.springboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RouterController {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
